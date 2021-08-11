0208f57072ec63632c0512992aa106f0d9828ecb
jinhyeok <ekdrn3330@naver.com>
2021-08-11 12:20:59 +0900

    210811start

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <Post :인스타데이터="인스타데이터" />
+      <Post :인스타데이터="인스타데이터" />
+      <Post :인스타데이터="인스타데이터" />
+  </div>
+</template>
+
+<script>
+import Post from './Post';
+
+export default {
+    name: 'Container',
+    components: {
+        Post,
+    },
+    props: {
+        인스타데이터 : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
